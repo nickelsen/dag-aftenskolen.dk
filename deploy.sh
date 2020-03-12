3d32c8fdb94b8f7e5035c26d6f3868fe04f9ebae
Anders Nickelsen <anders.nickelsen@gmail.com>
2020-03-12 13:25:19 +0100

    Cookies and corona.

--- deploy.sh
+++ deploy.sh
@@ -2,3 +2,4 @@
 
 aws s3 sync _site/ s3://dag-aftenskolen.dk --content-type "text/html; charset=utf-8" --delete --cache-control "max-age=0" --exclude "*.sh" $@
 aws s3 cp _site/assets/css/main-2019.css s3://dag-aftenskolen.dk/assets/css/main-2019.css --content-type="text/css" $@
+aws cloudfront create-invalidation --distribution-id EO900804BMV13 --paths /\* $@
